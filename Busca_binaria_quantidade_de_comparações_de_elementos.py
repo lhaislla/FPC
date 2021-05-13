f4dd82062c1ec2329d15e409c5b3d1075fd93953
Lhaíslla Cavalcanti <lhaislla@gmail.com>
2021-05-13 18:24:59 -0300

    Update Busca_binaria_quantidade_de_comparações_de_elementos.py
    Busca binaria, que retorna a quantidade de comparações do elemento buscado.

--- Busca_binaria_quantidade_de_comparações_de_elementos.py
+++ Busca_binaria_quantidade_de_comparações_de_elementos.py
@@ -4,26 +4,27 @@ def busca(lista, item, qtd):
         return qtd
     else:
         if len(lista) % 2 == 0:
-            meio = len(lista)// 2
+            meio = (len(lista)// 2) -1
         elif len(lista)% 2 != 0 :
-            meio =(len(lista) // 2) + 1
-        if lista[meio-1] == item:
+            meio =(len(lista) // 2) 
+        if lista[meio] == item:
             qtd += 1
             return qtd
         else:
-            if item < lista[meio-1]:
+            if item < lista[meio]:
                 qtd += 1
-                return busca(lista[:meio-1], item, qtd)
+                return busca(lista[:meio], item, qtd)
             else:
                 qtd += 1
-                return busca(lista[meio:], item, qtd)
+                return busca(lista[meio+1:], item, qtd)
     return qtd
+
+# Tratamento da entrada: 
 '''A entrada deve ser:  
-15 9 11 15 20 25 29 32 41 44 50 60 65 72 91 99 60 45 20 8 9 19
+exemplo: 15 9 11 15 20 25 29 32 41 44 50 60 65 72 91 99 60 45 20 8 9 19
 Sendo o primeiro número a quantidade de elementos da lista,
 Após os elementos da lista, terão os elementos a serem buscados'''
 
-# Tratamento da entrada: 
 entrada = input("Digite sua entrada: ").split()
 entrada = [int(x) for x in entrada]
 tam_busca = entrada[0]
@@ -31,13 +32,10 @@ lista = entrada[1:tam_busca + 1]
 lista_itens= entrada[tam_busca +1 : len(entrada)]
 resultado =[]
 
-
 # Chamada da função até que a lista_Itens esteja vazia: 
-while True:
-    if lista_itens != []:
-        remove = lista_itens.pop(0)
-        resultado.append(busca(lista, remove, 0))      
-    else:
-        entrega = ' '.join(str(e) for e in resultado)
+while lista_itens != []:
+    remove = lista_itens.pop(0)
+    resultado.append(busca(lista, remove, 0))      
+    entrega = ' '.join(str(e) for e in resultado)
+    if lista_itens == []:
         print(entrega)
-        break
